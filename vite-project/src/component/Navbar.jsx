c99319b0d400f9968cbe4d94b0c2ef40327649e9
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-08-15 15:21:05 +0500

    v-02

---
+++ vite-project/src/component/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+
+function Navbar() {
+    return (
+        <header>
+
+            <ul className='navMenu' >
+                <li>home</li>
+                <li>about</li>
+                <li>contact</li>
+            </ul>
+
+        </header>
+    )
+}
+
+export default Navbar
