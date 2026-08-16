312256519b3499ae692a1f4d3ff2303c42a3100c
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-08-16 23:15:30 +0500

    v-05

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <>
       <div className="parent">
-        <p>My name is arshid</p>
-        <a href="https://arshidiqbal.net">Contact</a>
+        <h1 className='h-1' >ARSHID IQBAL</h1>
+        <p className='p-1' >Helping businesses establish a strong online presence through modern, high-performing WordPress websites.</p>
+        <a href="https://arshidiqbal.net" target='_blank' className='btn-1' >Contact</a>
       </div>
     </>
   );
