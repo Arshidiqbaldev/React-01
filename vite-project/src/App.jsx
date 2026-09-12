f6e6fbb53b006bcd1c6c1eb4c06deb5c5773ea0a
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-09-12 13:20:29 +0500

    v-019

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,16 +2,43 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import Logger from "./component/Logger";
 import Timer from "./component/Timer";
-import "./App.css"
+import "./App.css";
 import Datafitch from "./component/Datafitch";
 function App() {
+  const numbs = [
+    {
+      userId: 1,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "qui est esse",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+  ];
+
+  console.log(numbs);
 
   return (
     <div className="parent">
-     {/* <Logger/> */}
-     {/* <Timer className="children"/> */}
-     <Datafitch/>
-     
+      {/* <Logger/> */}
+      {/* <Timer className="children"/> */}
+      {/* <Datafitch/> */}
+      <ul>
+        {numbs.map((n) => (
+          <li key={n.id}>{n.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
