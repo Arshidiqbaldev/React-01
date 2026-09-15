bc34f4040eae0afadb1fa25323fffc06a4fdbdf6
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-09-15 16:07:57 +0500

    v0020

--- vite-project/src/component/Profile.jsx
+++ vite-project/src/component/Profile.jsx
@@ -1,9 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { UserContext } from "../App";
 
 function Profile() {
   const { theme, setTheme } = useContext(UserContext);
+  const [my, setCar] = useState({
+    car: "bmw",
+    model: "1990",
+    color: "red",
+  });
 
   function toggleTheme() {
     if (theme === "light") {
@@ -11,11 +16,18 @@ function Profile() {
     } else {
       setTheme("light");
     }
+
+    setCar((previousState) => {
+      return { ...previousState, color: "blue" , model: "2000" , car : "Audi" };
+    });
   }
   return (
     <div className="children">
-      <h1 className={theme==="light"? "c1" : "c2"} >{theme}</h1>
-      <p className={theme==="light"? "c1" : "c2"} ></p>
+      <h1 className={theme === "light" ? "c1" : "c2"}>{theme}</h1>
+      <p className={theme === "light" ? "c1" : "c2"}>
+        {my.car} , {my.model} , {my.color}
+      </p>
+
       <button className="btn" onClick={toggleTheme}>
         Change
       </button>
